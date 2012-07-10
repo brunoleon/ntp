0ab8aa5c1eec043aee8c4ae42bb2b8be82ac47ab
Bruno Léon <bruno.leon@unyonsys.com>
2012-07-10 21:54:55 +0200

    Add proper /etc/default/ntpdate so that the clock is set at boot time.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,13 +2,24 @@
 #
 # * Setup proper time management
 class ntp (
-  $server_region = false
+  $server_region  = '',
+  $ntpdate_server = '91.189.94.4',
 ) {
+
+  validate_string( $server_region )
+
   if ! $::ec2_instance_id {
     case $::osfamily {
       /(?i-mx:debian)/: {
         class { 'ntp::openntpd':
           server_region => $ntp::server_region
+        }
+        if ! is_ip_address( $ntpdate_server ) {
+          fail( 'You should provide an IP and not a hostname here. NTPDATE fails DNS resolution at boot time.')
+        }
+        file { '/etc/default/ntpdate':
+          ensure  => file,
+          content => template( 'ntp/default_ntpdate.erb' ),
         }
       }
       /(?i-mx:redhat)/: {
--- manifests/openntpd.pp
+++ manifests/openntpd.pp
@@ -2,7 +2,7 @@
 #
 # * Installs ntp daemon, optionally using the closest server as specified
 class ntp::openntpd (
-  $server_region = false
+  $server_region,
 ) {
   package { 'openntpd' :
     ensure => present,
